273aa13889548fcc6a2c3f22f7fe08101c30fc40
Derin <106310334+derinbuldanli@users.noreply.github.com>
2023-03-07 22:00:20 +0300

    Add files via upload

---
+++ MyFirstLabScript.py
@@ -0,0 +1,30 @@
+student_name = input("Enter your name: ")
+print('Hello', student_name)
+student_id = input('Enter your student id:')
+print('your student id is:', student_id)
+
+
+var1 = int(input("Enter the value of var1: "))
+var2 = int(input("Enter the value of var2: "))
+print('your values are :', var1, 'and', var2)
+
+sum = var1 + var2
+print(sum)
+diff = var1 - var2
+print(diff)
+prod = var1 * var2
+print(prod)
+
+
+studentname = input("Enter student name:")
+labgrade = float(input("Enter lab grade : "))
+midtermgrade = float(input("Enter midterm grade : "))
+finalgrade = float(input("Enter final grade: "))
+lastscore = 0.25 * labgrade + 0.35 * midtermgrade + 0.4 * finalgrade
+print('Name:', studentname)
+print('Lab:', labgrade)
+print('Midterm:', midtermgrade)
+print('Final:',  finalgrade)
+print('Last score:', lastscore)
+
+print(' * \n ** \n *** \n ** \n * ')
